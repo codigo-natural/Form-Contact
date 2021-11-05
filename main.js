9479e3846d0c6bd24a585679ad1cea2e64e2db47
Code-Nature <73039901+codigo-natural@users.noreply.github.com>
2021-11-05 16:06:22 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,36 @@
+const formulario = document.getElementById('formulario');
+const inputs = document.querySelectorAll('#formulario input');
+
+const expresiones = {
+    nombre: /^[a-zA-ZÀ-ÿ\s]{1,40}$/, // Letras y espacios, pueden llevar acentos.
+    correo: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
+    telefono: /^\d{7,14}$/ // 7 a 14 numeros.
+}
+
+const validarFormulario = (e) => {
+    switch (e.target.name) {
+        case "fullname":
+                if(expresiones.nombre.test(e.terget.value)){
+
+                }
+            break;
+            case "email":
+
+            break;
+            case "phone":
+            
+            break;
+            case "affair":
+            
+            break;
+    };
+}
+
+inputs.forEach((input) => {
+    input.addEventListener('keyup', validarFormulario);
+    input.addEventListener('blur', validarFormulario);
+});
+
+formulario.addEventListener('submit', (e) => {
+    e.preventDefault();
+});
